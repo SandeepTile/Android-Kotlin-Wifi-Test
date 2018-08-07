7ee46d8bc6fa3b0fe096de3d7fd7253a5718d443
sandeeptile <sandeeptile@gmail.com>
2018-08-07 15:35:13 +0530

    first commit

---
+++ app/src/main/java/com/example/sandy/kotlin_wifi_test/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.sandy.kotlin_wifi_test
+
+import android.content.Context
+import android.net.wifi.WifiManager
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Vibrator
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
